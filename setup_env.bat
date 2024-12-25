d3b285e46ff7d7d07f99ef37c3f1e5c10b680cf8
BrianHuang <45776140+BrianHuang880507@users.noreply.github.com>
2024-12-25 15:30:17 +0800

    Add files via upload

--- setup_env.bat
+++ setup_env.bat
@@ -1,3 +1,24 @@
 @echo off
-call conda init
-conda create --name prolifmsc python=3.10 -y && conda activate prolifmsc && python -m pip install cellpose && pip install pyimagej && pip install torch torchvision torchaudio && echo Environment setup complete!
+:: 啟動 Anaconda Prompt 並執行命令
+echo =======================================
+echo Anaconda Environment Setup Script
+echo =======================================
+
+:: 切換到 Anaconda Prompt
+call %USERPROFILE%\anaconda3\Scripts\activate.bat
+
+:: 創建名為 prolifmsc 的虛擬環境並指定 Python 版本
+conda create --name prolifmsc python=3.10 -y
+
+:: 激活虛擬環境
+conda activate prolifmsc
+
+:: 安裝必要的套件
+python -m pip install cellpose
+pip install pyimagej
+pip3 install torch torchvision torchaudio
+
+echo =======================================
+echo Environment setup completed!
+echo =======================================
+pause
